01fc3ca0ac1e16f0da6fa6fcfd0b6d7b91c0a0be
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-11-12 21:01:47 +0100

    chore: update app module imports

--- services/template_service/src/app.module.ts
+++ services/template_service/src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TemplatesModule } from './templates/templates.module';
 import { HealthModule } from './health/health.module';
 import { PrismaService } from './config/prisma.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { max } from 'class-validator';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TemplatesModule, HealthModule],
+  imports: [
+    TemplatesModule,
+    HealthModule,
+    CacheModule.register({
+      max: 1000,
+      ttl: 0,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
