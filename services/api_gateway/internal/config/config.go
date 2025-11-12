6ce1fd2230eb6adab9c5eeb302abcb199622a229
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-11-12 13:47:57 +0100

    feat(config): add internal configurations, db connections, env setup

---
+++ services/api_gateway/internal/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+// Config holds the application configuration.
+type Config struct {
+	Port               string
+	DatabaseURL        string
+	RedisURL           string
+	RabbitMQURL        string
+	UserServiceURL     string
+	TemplateServiceURL string
+	UserPrefCacheTTL   time.Duration
+}
+
+// Load loads the configuration from environment variables.
+func Load() (*Config, error) {
+	// Load .env file if it exists
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	return &Config{
+		Port:               getEnv("PORT", "8080"),
+		DatabaseURL:        getEnv("DATABASE_URL", ""),
+		RedisURL:           getEnv("REDIS_URL", ""),
+		RabbitMQURL:        getEnv("RABBITMQ_URL", ""),
+		UserServiceURL:     getEnv("USER_SERVICE_URL", ""),
+		TemplateServiceURL: getEnv("TEMPLATE_SERVICE_URL", ""),
+		UserPrefCacheTTL:   getEnvAsDuration("USER_PREF_CACHE_TTL", 5*time.Minute),
+	}, nil
+}
+
+// getEnv retrieves an environment variable or returns a default value.
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
+
+func getEnvAsDuration(key string, defaultValue time.Duration) time.Duration {
+	if value, exists := os.LookupEnv(key); exists {
+		if d, err := time.ParseDuration(value); err == nil {
+			return d
+		}
+		log.Printf("invalid duration for %s; using default %s", key, defaultValue)
+	}
+	return defaultValue
+}
